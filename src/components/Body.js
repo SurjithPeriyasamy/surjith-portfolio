1c30c8bc5ae63801d11d7add0645d88769573921
Surjith <mahindhiran005@gmail.com>
2023-09-07 17:05:48 +0530

    create app and install & configure tailwind

---
+++ src/components/Body.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import MainContainer from "./MainContainer";
+
+const Body = () => {
+  return (
+    <div>
+      <Header />
+      <MainContainer />
+    </div>
+  );
+};
+
+export default Body;
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <div className="fixed right-0 left-0 flex justify-between bg-white shadow-lg py-5 px-14 text-zinc-700 items-center">
+      <div>
+        <h1 className="font-bold text-xl">Surjith.dev</h1>
+      </div>
+      <ul className="flex gap-5 font-semibold text-lg">
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contacts</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
