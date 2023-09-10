225c74ad70b369588955376259e5d37f1d5ae83b
Surjith <mahindhiran005@gmail.com>
2023-09-10 20:58:25 +0530

    install & configure plugin for scroll&import font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,13 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      transformOrigin: {
-        "top-left-round": "0 100px 0 100px",
-      },
-      rotate: {
-        17: "8.3deg",
-      },
       animation: {
         "spin-slow": "spin 8s linear infinite",
       },
+      fontFamily: {
+        Poppins: ["Poppins"],
+      },
     },
   },
-  plugins: [],
+  plugins: [require("tailwind-scrollbar")({ nocompatible: true })],
 };
