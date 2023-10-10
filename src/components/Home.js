5d98ebf4c0ba2d9f4c11763f07141ec8d9483ce3
Surjith <mahindhiran005@gmail.com>
2023-10-10 13:10:57 +0530

    added stacks objects for image alt and src

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,12 +6,30 @@ import GitHub from "./GitHub";
 import LinkedIn from "./LinkedIn";
 const Home = () => {
   const techStackLogos = [
-    "https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_html_icon_130541.png",
-    "https://cdn.icon-icons.com/icons2/844/PNG/512/CSS3_icon-icons.com_67069.png",
-    "https://cdn.icon-icons.com/icons2/2415/PNG/512/javascript_original_logo_icon_146455.png",
-    "https://cdn.icon-icons.com/icons2/2415/PNG/512/react_original_logo_icon_146374.png",
-    "https://cdn.icon-icons.com/icons2/2415/PNG/512/redux_original_logo_icon_146365.png",
-    "https://cdn.icon-icons.com/icons2/2699/PNG/512/tailwindcss_logo_icon_167923.png",
+    {
+      name: "HTML",
+      logo: "https://cdn2.icon-icons.com/icons2/2107/PNG/512/file_type_html_icon_130541.png",
+    },
+    {
+      name: "CSS3",
+      logo: "https://cdn2.icon-icons.com/icons2/844/PNG/512/CSS3_icon-icons.com_67069.png",
+    },
+    {
+      name: "Javascript",
+      logo: "https://cdn2.icon-icons.com/icons2/2108/PNG/512/javascript_icon_130900.png",
+    },
+    {
+      name: "React",
+      logo: "https://cdn2.icon-icons.com/icons2/2415/PNG/512/react_original_logo_icon_146374.png",
+    },
+    {
+      name: "Redux",
+      logo: "https://cdn2.icon-icons.com/icons2/2415/PNG/512/redux_original_logo_icon_146365.png",
+    },
+    {
+      name: "Tailwind CSS",
+      logo: "https://cdn2.icon-icons.com/icons2/2699/PNG/512/tailwindcss_logo_icon_167923.png",
+    },
   ];
 
   return (
@@ -72,8 +90,8 @@ const Home = () => {
               <li key={i} className="bg-white shadow-xl rounded-full p-3 ">
                 <img
                   className="h-9 hover:rotate-[360deg] duration-500"
-                  alt="html"
-                  src={stack}
+                  alt={stack.name}
+                  src={stack.logo}
                 />
               </li>
             ))}
