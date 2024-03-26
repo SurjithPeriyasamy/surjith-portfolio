2b1a44375f4fbc2e915c6961e2e1cf7d770a0ebb
Surjith <mahindhiran005@gmail.com>
2024-03-26 15:17:55 +0530

    new resume link

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -33,4 +33,4 @@ export const TECH_STACK_LOGOS = [
 ];
 
 export const RESUME_LINK =
-  "https://drive.google.com/file/d/1E7oBij-BBd2QdaD_J1CGz2lAaxSr3Ld3/view";
+  "https://drive.google.com/file/d/1XNkdr_0So9okONBUiieyNwOQH4lruqoQ/view";
